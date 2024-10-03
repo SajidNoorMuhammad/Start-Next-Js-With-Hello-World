e079678c22457a123173e32520d8656a7e5e0fef
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-10-03 13:20:39 +0500

    update.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+// next.config.js
+
+module.exports = {
+    basePath: '/first', // Replace with your GitHub repo name
+};
+
 const nextConfig = {};
 
 export default nextConfig;
